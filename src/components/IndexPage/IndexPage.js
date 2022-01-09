1a673bc58bef9634bfc8b6743fd13e7cc5308bfe
zoozxc456 <zoozxc456@gmail.com>
2022-01-09 14:34:25 +0800

    rebuilding LineLoginService

--- src/components/IndexPage/IndexPage.js
+++ src/components/IndexPage/IndexPage.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 /* Import Images */
 import banner from '../../assets/images/banner.png'
 import LineLogin from './LineLoginButton'
+import LoginService from '../../services/Login/LoginService';
 const style = {
     width: "100vw"
 }
@@ -10,17 +11,9 @@ const IndexPageComponent = () => {
     const [payload, setPayload] = useState(null);
     const [idToken, setIdToken] = useState(null);
 
-    // LINE LOGIN CONFIG
-    const clientID = "1656721614",
-        clientSecret="bec02e7cf2fb625f1cdaef87d54cfb1a",
-        state="12345abc",
-        nonce="09876xyz",
-        redirectURI="http://localhost:3000/",
-        scope="profile openid email";
-
     return (
         <>
-            <LineLogin
+            {/* <LineLogin
                 clientID={clientID}
                 clientSecret={clientSecret}
                 state={state}
@@ -29,6 +22,10 @@ const IndexPageComponent = () => {
                 scope={scope}
                 setPayload={setPayload}
                 setIdToken={setIdToken}
+            /> */}
+            <LineLogin
+                onSuccess={LoginService.LineLoginSuccess}
+                onFailure={LoginService.LineLoginFailure}
             />
             <img src={banner} alt={'banner'} style={style} />
         </>

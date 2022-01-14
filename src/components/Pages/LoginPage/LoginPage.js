156c2d415ced75a1e10a090cf3430d3a711ddf2d
zoozxc456 <zoozxc456@gmail.com>
2022-01-14 14:33:57 +0800

    hook with LineLogin and add appearance

---
+++ src/components/Pages/LoginPage/LoginPage.js
@@ -0,0 +1,13 @@
+import { Form } from "react-bootstrap";
+import LineLogin from "./LineLoginButton";
+
+const LoginPage = (props) => {
+    return (
+        <Form>
+            <LineLogin />
+        </Form>
+    )
+
+}
+
+export default LoginPage;

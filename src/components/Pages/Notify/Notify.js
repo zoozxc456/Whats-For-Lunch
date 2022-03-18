d6ae3d9f2af847acbc30f2dd2825ec9601ac555a
jasmy <jasmy0910@gmail.com>
2022-03-18 18:49:29 +0800

    update notify

--- src/components/Pages/Notify/Notify.js
+++ src/components/Pages/Notify/Notify.js
@@ -1,4 +1,5 @@
-import logo from '../../Assets/images/logo.png';
+import notification from '../../Assets/images/notification.png';
+import notification2 from '../../Assets/images/notification2.png';
 import './Notify.css';
 import React, { useState } from 'react';
 import { Col, Row, Button } from "react-bootstrap";
@@ -7,17 +8,26 @@ import { Col, Row, Button } from "react-bootstrap";
 
 const NotifyComponent = () => {
     const [height, setHeight] = useState('60px');
-
-    const changeHeight = () => {
+    const [width, setWidth] = useState('60px');
+    const [radius, setRadius] = useState('50px');
+    const changeSize = () => {
         console.log('clicked')
-        setHeight('600px')
+        setHeight('50vh')
+        setWidth('300px')
+        setRadius('10px')
+    }
+    const changeSize2 = () => {
+        console.log('clicked')
+        setHeight('60px')
+        setWidth('60px')
+        setRadius('50px')
     }
     return (
 
-        <div id="notify" className='' style={{height:height}} onClick={changeHeight}>
+        <div id="notify" className='' style={{height:height,width:width,borderRadius:radius}} onClick={changeSize} onMouseLeave={changeSize2}>
             <div className='title p-2'>
                 {/* 通知 */}
-                <img src={logo} className="" alt="line" style={{ width: '35px' }} />
+                <img src={notification} className="" alt="notification" style={{ width: '35px' }} />
             </div>
             <div>
 

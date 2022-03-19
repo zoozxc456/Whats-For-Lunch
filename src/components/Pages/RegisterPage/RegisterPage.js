ab5d8422f47e10d8e5345b4b5fe6022ceca3d251
jasmy <jasmy0910@gmail.com>
2022-03-19 21:41:57 +0800

    update forget password component

--- src/components/Pages/RegisterPage/RegisterPage.js
+++ src/components/Pages/RegisterPage/RegisterPage.js
@@ -11,25 +11,23 @@ import Notify from '../Notify/Notify';
 
 const RegisterPage = () => {
     const [content, setContent] = useState(null);
-    const [ForgetPwdContent, setForgetPwdContent] = useState(null);
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [username, setUsername] = useState("");
     const [fieldErrorList, setFieldErrorList] = useState([]);
 
-    const showForgetPwd = () => {
-        console.log('showForgetPwd1')
-        setForgetPwdContent(ForgetPwd1);
-        setContent(ForgetPwdComponent);
+    const showForgetPwd1 = () => {
+        setContent(ForgetPwdComponent1);
     }
     const showForgetPwd2 = () => {
-        console.log('showForgetPwd2')
-        setForgetPwdContent(ForgetPwd2);
+        setContent(ForgetPwdComponent2);
     }
     const showForgetPwd3 = () => {
-        console.log('showForgetPwd3')
-        setForgetPwdContent(ForgetPwd3);
+        setContent(ForgetPwdComponent3);
+    }
+    const showForgetPwd4 = () => {
+        setContent(ForgetPwdComponent4);
     }
     const showRegister = () => {
         setContent(RegisterComponent)
@@ -104,115 +102,21 @@ const RegisterPage = () => {
         if (window.location.search === '?content=register') {
             showRegister()
         } else if (window.location.search === '?content=forgetpassword') {
-            showForgetPwd()
+            showForgetPwd1()
         } else {
             window.location.href = "/"
         }
     }, []);
-    const ForgetPwd3 = (
-        <Row className="m-0">
-            <Form id="ForgetPasswordForm" className="align-self-center mx-auto w-75">
-                <Form.Group as={Col} className={"mt-2"}>
-                    <Form.Label>密碼</Form.Label>
-                    <Form.Control
-                        type="password"
-                        required
-                        placeholder="6+ Characters, 1 Capital letter"
-                        onChange={handlePasswordChangeEvent}
-                        isInvalid={handleIsInvalid("Password")}
-                    />
-                    <Form.Control.Feedback type="invalid">
-                        Please Enter Your Password
-                    </Form.Control.Feedback>
-                </Form.Group>
+    const ForgetPwdComponent4 = (
+        <div id="forgetpwd">
 
-                <Form.Group as={Col} className={"mt-2"}>
-                    <Form.Label>確認密碼</Form.Label>
-                    <Form.Control
-                        type="password"
-                        required
-                        placeholder="6+ Characters, 1 Capital letter"
-                        onChange={handlePasswordChangeEvent}
-                        isInvalid={handleIsInvalid("Password")}
-                    />
-                    <Form.Control.Feedback type="invalid">
-                        Please Enter Your Password
-                    </Form.Control.Feedback>
-                </Form.Group>
-
-
-
-                <ButtonGroup className="w-100 mt-3">
-                    <Button type="button" className="me-1" onClick={handleSignUpClick}> 下一步 <span></span></Button>
-                </ButtonGroup>
-
-
-            </Form>
-        </Row>
+            <div className='step4 py-auto'>
+                <h3>密碼已變更！請重新登入</h3>
+                <div className='txt'>跳轉至首頁(3)</div>
+            </div>
+        </div>
     );
-    const ForgetPwd2 = (
-        <Row className="m-0">
-            <Form id="ForgetPasswordForm" className="align-self-center mx-auto w-75">
-                <Form.Group as={Col} className={""}>
-                    <div className="">
-                        <Form.Label className="me-3">輸入驗證碼</Form.Label>
-                    </div>
-
-                    <Form.Control
-                        type="email"
-                        required
-                        placeholder="輸入驗證碼"
-                        onChange={handleEmailChangeEvent}
-                        isInvalid={handleIsInvalid("Email")}
-                    />
-                    <Form.Control.Feedback type="invalid">
-                        Please Enter Your E-mail
-                    </Form.Control.Feedback>
-                </Form.Group>
-
-
-
-                <ButtonGroup className="w-100 mt-3">
-                    <Button type="button" className="me-1" onClick={showForgetPwd3}> 下一步 <span></span></Button>
-                </ButtonGroup>
-
-
-            </Form>
-        </Row>
-    );
-    const ForgetPwd1 = (
-        <Row className="m-0">
-            <Form id="ForgetPasswordForm" className="align-self-center mx-auto w-75">
-                {/* E-mail Field */}
-                <Form.Group as={Col} className={""}>
-                    <div className="">
-                        <Form.Label className="me-3">E-mail</Form.Label>
-                    </div>
-
-                    <Form.Control
-                        type="email"
-                        required
-                        placeholder="name@mail.com"
-                        onChange={handleEmailChangeEvent}
-                        isInvalid={handleIsInvalid("Email")}
-                    />
-                    <Form.Control.Feedback type="invalid">
-                        Please Enter Your E-mail
-                    </Form.Control.Feedback>
-                </Form.Group>
-
-
-
-                <ButtonGroup className="w-100 mt-3">
-                    <Button type="button" className="me-1" onClick={showForgetPwd2}> 下一步 <span></span></Button>
-                </ButtonGroup>
-
-
-            </Form>
-        </Row>
-    );
-
-    const ForgetPwdComponent = (
+    const ForgetPwdComponent3 = (
         <div id="forgetpwd">
             <Row className="text-center">
                 <Col xs={2} className='my-5 fs-3 p-0'>
@@ -226,7 +130,150 @@ const RegisterPage = () => {
 
                 </Col>
             </Row>
-            {ForgetPwdContent}
+            <Row className='number mx-auto'>
+                <Col xs={1} className={`text-center p-1 mx-2 ${content ? '' : ''}`}>1</Col>
+                <Col xs={1} className='text-center p-1 mx-2'>2</Col>
+                <Col xs={1} className='text-center p-1 mx-2 now'>3</Col>
+            </Row>
+            <Row className="m-0">
+                <Form id="ForgetPasswordForm" className="align-self-center mx-auto w-75">
+                    <Form.Group as={Col} className={"mt-2"}>
+                        <Form.Label>輸入新密碼</Form.Label>
+                        <Form.Control
+                            type="password"
+                            required
+                            placeholder="6+ Characters, 1 Capital letter"
+                            onChange={handlePasswordChangeEvent}
+                            isInvalid={handleIsInvalid("Password")}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            Please Enter Your Password
+                        </Form.Control.Feedback>
+                    </Form.Group>
+
+                    <Form.Group as={Col} className={"mt-2"}>
+                        <Form.Label>確認新密碼</Form.Label>
+                        <Form.Control
+                            type="password"
+                            required
+                            placeholder="6+ Characters, 1 Capital letter"
+                            onChange={handlePasswordChangeEvent}
+                            isInvalid={handleIsInvalid("Password")}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            Please Enter Your Password
+                        </Form.Control.Feedback>
+                    </Form.Group>
+
+
+
+                    <ButtonGroup className="w-100 mt-3">
+                        <Button type="button" className="me-1" onClick={showForgetPwd4}> 下一步 <span></span></Button>
+                    </ButtonGroup>
+
+
+                </Form>
+            </Row>
+        </div>
+    );
+    const ForgetPwdComponent2 = (
+        <div id="forgetpwd">
+            <Row className="text-center">
+                <Col xs={2} className='my-5 fs-3 p-0'>
+                    <img src={back} className={`back`} alt="back" onClick={handleLogInClick} />
+                </Col>
+                <Col xs={8} className='my-5 fs-3 p-0'>
+                    <b>忘記密碼</b>
+                    <span> Forget Password</span>
+                </Col>
+                <Col xs={2} className='my-5 fs-3 p-0'>
+
+                </Col>
+            </Row>
+            <Row className='number mx-auto'>
+                <Col xs={1} className={`text-center p-1 mx-2${content ? '' : ''}`}>1</Col>
+                <Col xs={1} className='text-center p-1 mx-2 now'>2</Col>
+                <Col xs={1} className='text-center p-1 mx-2'>3</Col>
+            </Row>
+            <Row className="m-0">
+                <Form id="ForgetPasswordForm" className="align-self-center mx-auto w-75">
+                    <Form.Group as={Col} className={""}>
+                        <div className="">
+                            <Form.Label className="me-3">輸入驗證碼</Form.Label>
+                        </div>
+
+                        <Form.Control
+                            type="email"
+                            required
+                            placeholder="輸入驗證碼"
+                            onChange={handleEmailChangeEvent}
+                            isInvalid={handleIsInvalid("Email")}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            Please Enter Your E-mail
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                    <div className='mt-2 code_text'>沒有收到驗證碼？(60)</div>
+
+                    <ButtonGroup className="w-100 mt-3">
+                        <Button type="button" className="me-1" onClick={showForgetPwd3}> 下一步 <span></span></Button>
+                    </ButtonGroup>
+
+
+                </Form>
+            </Row>
+
+        </div>
+    );
+
+    const ForgetPwdComponent1 = (
+        <div id="forgetpwd">
+            <Row className="text-center">
+                <Col xs={2} className='my-5 fs-3 p-0'>
+                    <img src={back} className={`back`} alt="back" onClick={handleLogInClick} />
+                </Col>
+                <Col xs={8} className='my-5 fs-3 p-0'>
+                    <b>忘記密碼</b>
+                    <span> Forget Password</span>
+                </Col>
+                <Col xs={2} className='my-5 fs-3 p-0'>
+
+                </Col>
+            </Row>
+            <Row className='number mx-auto'>
+                <Col xs={1} className={`text-center p-1 mx-2 now ${content ? '' : ''}`}>1</Col>
+                <Col xs={1} className='text-center p-1 mx-2'>2</Col>
+                <Col xs={1} className='text-center p-1 mx-2'>3</Col>
+            </Row>
+            <Row className="m-0">
+                <Form id="ForgetPasswordForm" className="align-self-center mx-auto w-75">
+                    {/* E-mail Field */}
+                    <Form.Group as={Col} className={""}>
+                        <div className="">
+                            <Form.Label className="me-3">輸入您註冊的E-mail</Form.Label>
+                        </div>
+
+                        <Form.Control
+                            type="email"
+                            required
+                            placeholder="name@mail.com"
+                            onChange={handleEmailChangeEvent}
+                            isInvalid={handleIsInvalid("Email")}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            Please Enter Your E-mail
+                        </Form.Control.Feedback>
+                    </Form.Group>
+
+
+
+                    <ButtonGroup className="w-100 mt-3">
+                        <Button type="button" className="me-1" onClick={showForgetPwd2}> 下一步 <span></span></Button>
+                    </ButtonGroup>
+
+
+                </Form>
+            </Row>
         </div>
     );
     const RegisterComponent = (

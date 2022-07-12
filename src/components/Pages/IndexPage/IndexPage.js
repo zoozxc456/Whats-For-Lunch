85a6a1ee5b3a1dae584203b14396c2a66491839f
zoozxc456 <zoozxc456@gmail.com>
2022-07-12 16:54:33 +0800

    add login service feature , still have bugs

--- src/components/Pages/IndexPage/IndexPage.js
+++ src/components/Pages/IndexPage/IndexPage.js
@@ -4,14 +4,48 @@ import rice from '../../Assets/images/rice.png';
 import google from '../../Assets/images/google.png';
 import line from '../../Assets/images/line.png';
 import './IndexPage.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Col, Row, Button } from "react-bootstrap";
+import axios from 'axios';
 
 
 const IndexPageComponent = () => {
     // const [payload, setPayload] = useState(null);
     // const [idToken, setIdToken] = useState(null);
     const [content, setContent] = useState(null);
+    // const email = useRef("");
+    // const password = useRef("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [disabled, setDisabled] = useState(false);
+    const [isInvalidClick, setIsInvalidClick] = useState("disabled");
+    const [isInvalidEmail, setIsInvalidEmail] = useState(false);
+    const [isInvalidPassword, setIsInvalidPassword] = useState(false);
+    // const emailValue = email.current.value;
+    // const passwordValue = password.current.value;
+    useEffect(() => {
+        // console.log(emailValue, passwordValue)
+        // if (emailValue !== undefined && passwordValue !== undefined) {
+        //     console.log("should remove disabled")
+        //     setIsInvalidClick("false")
+        // }
+        console.log(email, password)
+        if (email.length <= 0 || password.length <= 0) {
+            // document.querySelector(".loginbtn").setAttribute("disabled",true)
+            setDisabled(true)
+
+        } else {
+            console.log(123)
+            // document.querySelector(".loginbtn").removeAttribute("disabled")
+            setDisabled(false)
+        }
+
+    }, [email, password, disabled]);
+
+    const checkLogin = () => {
+
+
+    }
 
     const removeContent = () => {
         setContent('')
@@ -34,21 +68,27 @@ const IndexPageComponent = () => {
             <div className='login mt-5'>
                 <b>登入</b>
                 <span>Login</span>
-    
+
             </div>
             <div className='my-1'>
-                <input className='py-1 ps-2' placeholder='電子郵件帳號'></input>
+                <input className='py-1 ps-2' placeholder='電子郵件帳號' onChange={(event) => setEmail(event.target.value)} ></input>
             </div>
             <div className='my-1'>
-                <input className='py-1 ps-2' placeholder='密碼' type={"password"}></input>
+                <input className='py-1 ps-2' placeholder='密碼' type={"password"} onChange={(event) => setPassword(event.target.value)}></input>
             </div>
             <div className='function_txt fs-6 my-2'>
                 <span onClick={handleRegister}>註冊</span> | <span onClick={handleForgetPassword}>忘記密碼？</span>
-    
+
             </div>
             <div className="line2 mx-auto"></div>
             <div className='mb-5'>
                 <div className="mb-2 mt-3">
+                    <div className="my-1">
+                        <button className="loginbtn" disabled ={disabled?true:""}>登入1</button>
+                        {/* <Button className="loginbtn fs-6" onClick={checkLogin}>
+                            登入
+                        </Button> */}
+                    </div>
                     <div className="my-1">
                         <Button className="google_btn fs-6">
                             <img src={google} className="me-1" alt="google" style={{ width: '17px' }} />
@@ -65,7 +105,7 @@ const IndexPageComponent = () => {
             </div>
         </Col>
     );
-    
+
     const AboutUsComponent = (
         < Col className="about_block" >
             <div className='my-5'>
@@ -78,8 +118,8 @@ const IndexPageComponent = () => {
             </div>
         </Col >
     );
-    
-    
+
+
     return (
 
         <div id="home" >
